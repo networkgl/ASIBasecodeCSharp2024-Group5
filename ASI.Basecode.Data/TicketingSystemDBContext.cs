08f0b7b4662b331616ebc2b93fa0a8bd68cac96f
Syomaii <chrischanjay29@gmail.com>
2024-10-05 15:27:09 +0800

    Update assign ticket

--- ASI.Basecode.Data/TicketingSystemDBContext.cs
+++ ASI.Basecode.Data/TicketingSystemDBContext.cs
@@ -46,12 +46,14 @@ namespace ASI.Basecode.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=TicketingSystemDB;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+                optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=TicketingSystemDB;Integrated Security=True;Trust Server Certificate=True");
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AS");
+
             modelBuilder.Entity<Article>(entity =>
             {
                 entity.ToTable("Article");
