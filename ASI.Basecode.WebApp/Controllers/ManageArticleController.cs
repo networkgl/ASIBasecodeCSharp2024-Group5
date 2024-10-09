80e884a609bfaff204c39f8ef3febc718bc536a7
Lord Leo Lagare <lordleolagare@gmail.com>
2024-10-10 00:01:17 +0800

    Update Delete Url in Index Views for ManageTicket, Add ManageArcticleController

---
+++ ASI.Basecode.WebApp/Controllers/ManageArticleController.cs
@@ -0,0 +1,44 @@
+﻿using ASI.Basecode.Data.Interfaces;
+using ASI.Basecode.Data.Models.CustomModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    public class ManageArticleController : BaseController
+    {
+        public IActionResult Index()
+        {
+            if (TempData["temp"] != null)
+            {
+                if (TempData["status"] as int? == 0)
+                {
+                    TempData["ResMsg"] = new AlertMessageContent()
+                    {
+                        Status = ErrorCode.Success,
+                        Message = "An article has deleted successfully!"
+                    };
+                }
+                else
+                {
+                    TempData["ResMsg"] = new AlertMessageContent()
+                    {
+                        Status = ErrorCode.Error,
+                        Message = "An error has occured upon deleting the article."
+                    };
+                }
+            }
+            if (User.Identity.IsAuthenticated)
+            {
+                var userId = Convert.ToInt32(User.FindFirst("UserId")?.Value);
+                var myTickets = _db.VwUserTicketViews.Where(m => m.UserId == userId).ToList();
+
+                return View(myTickets);
+            }
+            return NotFound();
+        }
+
+
+    }
+}
