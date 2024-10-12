d936fd5286e892a1111b16c46af6c550b5143f58
Nowlly123 <nowlsilawan18@gmail.com>
2024-10-12 12:04:18 +0800

    AddManage Article and updated the manage user delete

--- ASI.Basecode.WebApp/Controllers/ManageArticleController.cs
+++ ASI.Basecode.WebApp/Controllers/ManageArticleController.cs
@@ -1,14 +1,22 @@
 ﻿using ASI.Basecode.Data.Interfaces;
+using ASI.Basecode.Data.Models;
 using ASI.Basecode.Data.Models.CustomModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq;
 using System;
+using System.Linq;
+using System.Security.Claims;
 
 namespace ASI.Basecode.WebApp.Controllers
 {
+    [Authorize(Policy = "AdminAndAgentPolicy")]
     public class ManageArticleController : BaseController
     {
-        public IActionResult Index()
+        public ManageArticleController(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        {
+        }
+        public IActionResult Index(string searchTerm)
         {
             if (TempData["temp"] != null)
             {
@@ -17,7 +25,7 @@ namespace ASI.Basecode.WebApp.Controllers
                     TempData["ResMsg"] = new AlertMessageContent()
                     {
                         Status = ErrorCode.Success,
-                        Message = "An article has deleted successfully!"
+                        Message = "An article has been deleted successfully!"
                     };
                 }
                 else
@@ -25,20 +33,110 @@ namespace ASI.Basecode.WebApp.Controllers
                     TempData["ResMsg"] = new AlertMessageContent()
                     {
                         Status = ErrorCode.Error,
-                        Message = "An error has occured upon deleting the article."
+                        Message = "An error has occurred upon deleting the article."
                     };
                 }
             }
-            if (User.Identity.IsAuthenticated)
-            {
-                var userId = Convert.ToInt32(User.FindFirst("UserId")?.Value);
-                var myTickets = _db.VwUserTicketViews.Where(m => m.UserId == userId).ToList();
+            var articles = _articleRepo.GetAll().ToList();
 
-                return View(myTickets);
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                articles = articles.Where(a => a.Content.Contains(searchTerm)).ToList();
             }
-            return NotFound();
+            return View(articles);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Article());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Article article)
+        {
+
+            article.UserId = Convert.ToInt32(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+            if (string.IsNullOrEmpty(article.Content))
+            {
+                TempData["ResMsg"] = "Please provide the article content.";
+                TempData["ResStatus"] = "Error";
+                return View(article);
+            }
+
+            if (article == null)
+            {
+                TempData["ResMsg"] = "An error has occurred upon creating the article.";
+                TempData["ResStatus"] = "Error";
+                return View(article);
+            }
+
+            var result = _articleRepo.Create(article);
+            TempData["ResMsg"] = result == ErrorCode.Success ? "Article created successfully!" : "An error occurred while creating the article.";
+            TempData["ResStatus"] = result == ErrorCode.Success ? "Success" : "Error";
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var article = _articleRepo.Get(id);
+
+            if (article == null)
+            {
+
+                TempData["ResMsg"] = new AlertMessageContent()
+                {
+                    Status = ErrorCode.Error,
+                    Message = "Article not found."
+                };
+                return RedirectToAction("Index");
+            }
+
+            return View(article);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Article article)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["ResMsg"] = "There was an error in your submission.";
+                TempData["ResStatus"] = ErrorCode.Error.ToString();
+                return View(article);
+            }
+
+            var result = _articleRepo.Update(article.ArticleId, article);
+            TempData["ResMsg"] = result == ErrorCode.Success ? "Updated Successfully!" : "An error occurred while updating the article.";
+            TempData["ResStatus"] = result == ErrorCode.Success ? ErrorCode.Success.ToString() : ErrorCode.Error.ToString();
+            return RedirectToAction("Index");
         }
 
 
+        public IActionResult Details(int id)
+        {
+            var article = _articleRepo.Get(id);
+            return View(article);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var result = _articleRepo.Delete(id);
+
+
+            if (result == ErrorCode.Success)
+            {
+                TempData["ResMsg"] = "Article deleted successfully!";
+                TempData["ResStatus"] = "success";
+            }
+            else
+            {
+                TempData["ResMsg"] = "Failed to delete the article. Please try again.";
+                TempData["ResStatus"] = "error";
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
